169ba43331a2e7c8c72c816a2eb9e71f2c80ce47
Denisa <florinachicarosa22@gmail.com>
2018-12-18 07:03:29 +0200

    To solve: ball problem

--- PingPong/PingPong.ino
+++ PingPong/PingPong.ino
@@ -2,9 +2,10 @@
 #include <LiquidCrystal.h>
 #define V0_PIN 9
 #define FIRST_PLAYER_PIN A0
-#define SECOND_PLAYER_PIN A1
-
-
+#define SECOND_PLAYER_PIN A5
+#define interval 250
+#define potSignal 100
+#define ballMovingTime 2000
 LiquidCrystal lcd(4, 5, 6, 7, 8, 13);
 
 
@@ -12,7 +13,7 @@ LedControl lc = LedControl(12, 11, 10, 1);
 
 void lcdPrint(int i, int index) {
   lcd.setCursor(0,0);
-  if(index == 1) {
+  if(index == 1) 
     lcd.print("Player One: ");
   else lcd.print("Player Two: ");
   lcd.setCursor(i,1);
@@ -20,6 +21,7 @@ void lcdPrint(int i, int index) {
   lcd.setCursor(11,1);
   lcd.print("START"); 
 }
+
 
 class Tab{
  protected:
@@ -57,21 +59,26 @@ class Player : public Tab{
     int index;
     int score;
   public:
-    Player(int pin):Tab(pin) {
-    index = 0;
+    Player(int pin, int i):Tab(pin) {
+    index = i;
     score = 0;
+  }
+  Player operator++() {
+    score++;
+    return *this;
   }
   int getScore() { return score; }
   
   bool startGame(long& previousMillis, long& previousValue, int& i) {
     long currentMillis = millis();
     long currentValue = analogRead(getPIN());
-    if(currentMillis - previousMillis > 250) {
-      if(currentValue > previousValue + 50) {
+   // Serial.println(currentValue);
+    if(currentMillis - previousMillis > interval) {
+      if(currentValue > previousValue + 40) {
         i++;
         previousValue = currentValue;
        }
-     lcdPrint(i);
+     lcdPrint(i, index);
      if(i > 11) {
      lcd.clear();
      return true;
@@ -80,52 +87,120 @@ class Player : public Tab{
     return false;
   }
   
-  void moveTab(int previousMillis, int previousValue) {
+  void moveTab(int& previousMillis, int& previousValue) {
     
     byte maxLeft = { B11110000 };
     byte maxRight = { B00001111 };
     long currentMillis = millis();
     long currentValue = analogRead(PIN);
-    if(currentMillis - previousMillis > 250) {
-      if(currentValue - previousValue > 150 && pos != maxLeft) {
+    //Serial.print(currentValue);
+    //Serial.print(" ");
+   // Serial.println(previousValue);
+    if(currentMillis - previousMillis > interval) {
+      if(currentValue - previousValue > potSignal && pos != maxLeft) {
         pos=pos<<1;
+        //Serial.println("here");
         previousValue = currentValue;
       }
-      else if(previousValue - currentValue > 150 && pos != maxRight) {
+      else if(previousValue - currentValue > potSignal && pos != maxRight) {
         pos = pos>>1;
+        //Serial.println("and here");
         previousValue = currentValue;
       }
       previousMillis = currentMillis;
-      if(pos == maxLeft || pos == maxRight)
-      previousValue = currentValue;
+      if(pos == maxLeft || pos == maxRight){
+       previousValue = currentValue;
+      }
     }
   }
   
 };
 
 class Ball {
-  byte position;
+  byte pos;
+  int line,column;
+  int dirL, dirC;
+  long previousMillis;  
+  byte maxLeft = { B10000000 };
+  byte maxRight = { B00000001 };
+  public:
+  Ball() {
+    pos = {B00001000};
+    line = 4;
+    dirL = 1; 
+    dirC = 1;
+    previousMillis = 0;
+  }
+  void ballInit(){
+    pos = {B00001000};
+    line = 4;
+    dirL = 1; 
+    dirC = 1;
+    previousMillis = 0;
+  }
   
-}
+  int getDirL(){
+    return dirL;
+  }
+  int getDirC(){
+    return dirC;
+  }
+  void moveBall() {
+    long currentMillis = millis();
+    if(currentMillis - previousMillis > ballMovingTime){
+    line = line + dirL;
+    if(dirC > 0) pos = pos>>1;
+    else pos = pos<<1;
+    previousMillis = currentMillis;
+    }
+  }
+  byte getPos(){
+    return pos;
+  }
+  byte getLine(){
+    return line;
+  }
+  void setDirL(int d){ dirL = d;}
+  void setDirC(int d){ dirC = d;}
+  void touchWall(){
+    if(pos == maxLeft){
+      if(dirL < 0)  dirC = -1;
+      else
+      if(dirC < 0)  dirL = 1;
+    }
+    if(pos == maxRight){
+      if(dirL < 0) {
+        dirL = -1;
+        dirC = -1;
+      }
+      else if(dirL > 0) 
+      dirC = 1;
+      }
+  }
+  
+};
 
 class Game {
   Player player1;
   Player player2;
-  long previousValue, previousMillis;
+  Ball ball;
   byte image[8];
   int arrow;
   int state; // 1-> start player1, 2->start player2, 3->play the game
   
   public:
-    Game(): player1(FIRST_PLAYER_PIN),player2(SECOND_PLAYER_PIN) {
+    long previousValue1, previousMillis1;
+    long previousValue2, previousMillis2;
+    Game(): player1(FIRST_PLAYER_PIN,1),player2(SECOND_PLAYER_PIN,2) {
       state = 1;
-      previousValue = previousMillis = 0;
+      previousMillis1 = previousValue1 = 0;
+      previousValue2 = previousMillis2 = 0;
       arrow = 0;
       byte img[8] = {
         B00000000,
         B01100110,
+        B01100110,
         B00000000,
-        B00010000,
         B00000000,
         B10000001,
         B01000010,
@@ -138,25 +213,26 @@ class Game {
       switch(state) {
         case 1: {
           bool done;
-          done = player1.startGame(previousMillis, previousValue, arrow);
+          done = player1.startGame(previousMillis1, previousValue1, arrow);
           if(done == true) {
             state = 2;
-            previousMillis = previousValue = arrow = 0;
+            previousMillis1 = previousValue1 = arrow = 0;
           }
           break;
         }
         case 2:{
           bool done;
-          done = player2.startGame(previousMillis, previousValue, arrow);
+          done = player2.startGame(previousMillis2, previousValue2, arrow);
           if(done == true) {
             state = 3;
-            previousMillis = previousValue = arrow = 0;
-            changeMatrix();
+            previousMillis2 = previousValue2 = arrow = 0;
+            phaseOne();
           }
           break;
         }
         case 3:{
           //afiseaza scorul
+          updateScore();
            break;
         }
       }
@@ -174,31 +250,139 @@ class Game {
         B00111100,
         B00000000,
         B00000000,
-        B00010000,
+        B00000000,
         B00000000,
         B00000000,
         B00000000,
         B00111100
-      }
+      };
       for( int i = 0; i < 8; i++)
         image[i] = img[i];
     }
     
     void updatePlayers() {
-      player1.moveTab();
-      player2.moveTab();
+      int a,b;
+      a =  previousMillis1;
+      b = previousValue1;
+      player1.moveTab(a,b);
+      previousMillis1 = a;
+      previousValue1 = b;
+     int c, d;
+      c =  previousMillis2;
+      d = previousValue2;
+      //player2.moveTab(previousMillis2, previousValue2);
+      player2.moveTab(c,d);
+      previousMillis2= c;
+      previousValue2 = d;
+
+      
       image[0] = player1.getPos();
+      printMatrix();
       image[7] = player2.getPos();
+      printMatrix();
     }
     
     void updateBall() {
-      
+      byte ballLine = ball.getLine();
+      image[ballLine]<<9;
+      ball.moveBall();
+      verifyBall();
+      byte ballPosition = ball.getPos();
+      ballLine = ball.getLine();
+      image[ballLine] = ballPosition;
     }
+    
+    void verifyBall(){
+    ball.touchWall();
+    //touch the paddle
+    if(ball.getLine() == 0) {
+      byte result;
+      byte posb, posp; 
+      posb = ball.getPos();
+      posp = player1.getPos();
+      result = posb | posp;
+      if(result == posp) {
+        // the ball touched the paddle -> change direction
+        ball.setDirL(1);
+        ball.setDirC(ball.getDirC() * (-1));
+      }
+      else {
+        //the ball is out of bounds
+       player2++; 
+       updateScore();
+       reset();
+      }
+    }
+    else if(ball.getLine() == 7) {
+      byte result, posb, posp; 
+      posb = ball.getPos();
+      posp = player2.getPos();
+      result = posb | posp;
+      if(result == posp) {
+        // the ball touched the paddle -> change direction
+        ball.setDirL(-1);
+        ball.setDirC(ball.getDirC() * (-1));
+      }
+      else {
+        //the ball is out of bounds
+       player1++;
+       updateScore(); 
+       reset();
+      }
+    }
+  }
     void updateScore() {
-      
+      lcd.setCursor(0,0);
+      lcd.print("Player1 Player2");
+      lcd.setCursor(3,1);
+      lcd.print(player1.getScore());
+      lcd.setCursor(12,1);
+      lcd.print(player2.getScore());
     }
     
-    
+    void reset(){
+      int win;
+      if(player1.getScore() >= 3)
+        win = 1;
+      if(player2.getScore() >= 3);
+        win = 2;
+        
+      winner(win);
+      phaseOne();
+      player1.setPos(B00111100);
+      player2.setPos(B00111100);
+      ball.ballInit();
+      
+      //replay();
+    }
+    void winner(int win){
+      lcd.clear();
+      delay(50);
+      if(win == 1) {
+      lcd.setCursor(1,0);
+      lcd.print("Winner: First");
+      lcd.setCursor(0,1);
+      lcd.print("Rotate=replay");
+      }
+      else 
+        if(win == 2) {
+          lcd.setCursor(1,0);
+          lcd.print("Winner: Second");
+          lcd.setCursor(0,1);
+          lcd.print("Rotate=replay");
+      }
+    }
+//    void replay() {
+//      lcd.setCursor(0,0);
+//      if(index == 1) 
+//      lcd.print("Player One: ");
+//      else lcd.print("Player Two: ");
+//      lcd.setCursor(i,1);
+//      lcd.print("->");
+//      lcd.setCursor(10,1);
+//      lcd.print("REPLAY"); 
+//      
+//    }
 };
 
 
@@ -212,6 +396,7 @@ void setup() {
   pinMode(A1, INPUT);
   pinMode(V0_PIN, OUTPUT);
   analogWrite(V0_PIN, 120); 
+ // Serial.begin(9600);
 }
 
 Game pong;
@@ -222,6 +407,7 @@ void loop() {
   if(play == false)  {
     play = pong.start();
   }
+  
   if(play == true) {
     pong.updatePlayers();
     pong.updateBall();
